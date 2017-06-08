591508cb6b819c72c80f659d472edf774aafa697
Lukas <lukas.schoepf@student.uibk.ac.at>
2017-06-09 00:17:55 +0200

    Plaintext input via serial added

--- aes/aes.ino
+++ aes/aes.ino
@@ -9,22 +9,32 @@ uint8_t buf2[16];
 void setup() {
   // put your setup code here, to run once:
   pinMode(OUTPIN, OUTPUT);
-  Serial.begin(57600);
+  Serial.setTimeout(10000); 
+  Serial.begin(115200);
 }
+
+uint8_t plaintext[16];
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.readBytesUntil('\n', plaintext, 16);
+
+  Serial.println("Plaintext:");
+  for(int i = 0; i<16; i++)
+      Serial.print(plaintext[i]);
+  Serial.println();
+      
   digitalWrite(OUTPIN,HIGH);
   Serial.println("Starting encryption");
-  AES128_ECB_encrypt(data, key, buf);
+  AES128_ECB_encrypt(plaintext, key, buf);
   Serial.println("Encrypted");
   digitalWrite(OUTPIN, LOW);
   AES128_ECB_decrypt(buf, key, buf2);
   Serial.println("Decrypted");
-  int same = !(memcmp(buf2, data, 16));
+  int same = !(memcmp(buf2, plaintext, 16));
   if(same)
     Serial.println("SUCCESS!");
   else
     Serial.println("FAILURE!");
-  delay(500);
+  //delay(500);
 }
